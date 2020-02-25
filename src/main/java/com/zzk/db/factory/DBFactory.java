268b88027db8dfeaa6dd9f72ff4b149d4bb437eb
李凯 <likai@kuaiyun.cn>
2020-02-25 12:41:09 +0800

    init

---
+++ src/main/java/com/zzk/db/factory/DBFactory.java
@@ -0,0 +1,13 @@
+package com.zzk.db.factory;
+
+/**
+ * [数据库工厂]
+ *
+ * @author likai
+ * @version 1.0
+ * @date 2020/2/25 0025 12:35
+ * @company Gainet
+ * @copyright copyright (c) 2020
+ */
+public class DBFactory {
+}
